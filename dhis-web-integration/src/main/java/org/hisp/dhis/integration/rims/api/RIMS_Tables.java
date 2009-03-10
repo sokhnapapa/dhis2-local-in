417f503408b39091d429d6a3936529bcafd9e906
Bharath <Bharath@hispindia>
2009-03-10 09:59:34 +0530

    Removed outdated indian modules

--- dhis-web-integration/src/main/java/org/hisp/dhis/integration/rims/api/RIMS_Tables.java
+++
@@ -1,7 +0,0 @@
-package org.hisp.dhis.integration.rims.api;
-
-public class RIMS_Tables 
-{
-	public static final String PROFILE_PHC_MST = "profile_phc_mst";
-	public static final String DISTRICT_MST = "district_mst";
-}
