d90aa55c23896fb980e896de2f181f0854b512cf
abyot <abyota@gmail.com>
2009-04-08 11:44:18 +0530

    - updating mobile module

---
+++ dhis-web-mobile/src/main/java/org/hisp/dhis/sandbox/mobileimport/action/MobileHomePageAction.java
@@ -0,0 +1,17 @@
+package org.hisp.dhis.sandbox.mobileimport.action;
+
+import java.io.File;
+
+import com.opensymphony.xwork.Action;
+
+public class MobileHomePageAction implements Action
+{
+
+    public String execute()
+        throws Exception
+    {
+        return SUCCESS;
+    }
+    
+
+}
