3b47f206925f847cb09ed4c833ac33ab9c60d2db
Jason P. Pickering <jason.p.pickering@gmail.com>
2011-12-26 10:07:59 +0000

    Updating copyright dates to 2004-2012.

--- dhis-web-reports-national/src/main/java/org/hisp/dhis/reports/datasetlock/action/GetDataSetsForLockAction.java
+++ dhis-web-reports-national/src/main/java/org/hisp/dhis/reports/datasetlock/action/GetDataSetsForLockAction.java
@@ -1,7 +1,7 @@
 package org.hisp.dhis.reports.datasetlock.action;
 
 /*
- * Copyright (c) 2004-2010, University of Oslo
+ * Copyright (c) 2004-2012, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
