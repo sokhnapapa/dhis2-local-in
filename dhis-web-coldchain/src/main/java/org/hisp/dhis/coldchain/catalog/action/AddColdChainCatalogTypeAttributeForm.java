f0235629cac011ffd4037c2787cc1d122bc2b159
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2012-04-18 14:54:36 +0530

    local/in coldchain catalog attribute type work in progress

---
+++ dhis-web-coldchain/src/main/java/org/hisp/dhis/coldchain/catalog/action/AddColdChainCatalogTypeAttributeForm.java
@@ -0,0 +1,32 @@
+package org.hisp.dhis.coldchain.catalog.action;
+
+import org.hisp.dhis.program.Program;
+
+import com.opensymphony.xwork2.Action;
+
+public class AddColdChainCatalogTypeAttributeForm  implements Action
+{
+    // -------------------------------------------------------------------------
+    // Dependencies
+    // -------------------------------------------------------------------------
+    
+    
+    // -------------------------------------------------------------------------
+    // Input/Output
+    // -------------------------------------------------------------------------
+    
+    
+    // -------------------------------------------------------------------------
+    // Action implementation
+    // -------------------------------------------------------------------------
+
+    public String execute()
+        throws Exception
+    {
+
+
+        return SUCCESS;
+    }
+    
+    
+}
---
+++ dhis-web-coldchain/src/main/java/org/hisp/dhis/coldchain/catalog/action/GetColdChainCatalogTypeAttributeListAction.java
@@ -0,0 +1,27 @@
+package org.hisp.dhis.coldchain.catalog.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class GetColdChainCatalogTypeAttributeListAction  implements Action
+{
+    // -------------------------------------------------------------------------
+    // Dependency
+    // -------------------------------------------------------------------------
+    
+    
+    // -------------------------------------------------------------------------
+    // Output
+    // -------------------------------------------------------------------------
+    
+    
+    // -------------------------------------------------------------------------
+    // Action implementation
+    // -------------------------------------------------------------------------
+
+    public String execute() throws Exception
+    {
+        
+        return SUCCESS;
+    }
+    
+}
